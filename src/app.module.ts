5de5a1b06b3680dfba4dd04696b9eb077a1de79e
FilipeHaniel <filipe.haniel@gmail.com>
2026-06-04 10:50:12 -0300

    feat: (db) connect prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TasksModule } from './tasks/tasks.module'
-import { StudySessionsModule } from './study-sessions/study-sessions.module';
-import { StreakModule } from './streak/streak.module';
-import { ReviewsModule } from './reviews/reviews.module';
+import { StudySessionsModule } from './study-sessions/study-sessions.module'
+import { StreakModule } from './streak/streak.module'
+import { ReviewsModule } from './reviews/reviews.module'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [TasksModule, StudySessionsModule, StreakModule, ReviewsModule],
+  imports: [
+    TasksModule,
+    StudySessionsModule,
+    StreakModule,
+    ReviewsModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
