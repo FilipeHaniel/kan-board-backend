f957376e754716bdbf01dbb41356de1ccdfe9c38
FilipeHaniel <filipe.haniel@gmail.com>
2026-06-04 11:29:42 -0300

    feat:create users module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+import { CreateUserDto } from './dto/create-user.dto'
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: CreateUserDto) {
+    return this.prisma.user.create({
+      data,
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true,
+      },
+    })
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  findById(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    })
   }
 }
