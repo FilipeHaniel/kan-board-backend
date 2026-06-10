a34a70b7da50513efe392fdb37572f9b10d3e9d9
FilipeHaniel <filipe.haniel@gmail.com>
2026-06-09 23:19:55 -0300

    fix: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -7,6 +9,18 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors()
 
+  const config = new DocumentBuilder()
+    .setTitle('Kan Board API')
+    .setDescription('API de estudos')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000)
 }
+
 bootstrap()
