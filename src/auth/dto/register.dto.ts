4fdce9c36777d0c94d0b78f8f96e6a808e1bcbc3
FilipeHaniel <filipe.haniel@gmail.com>
2026-06-10 13:22:17 -0300

    fix: uograde dtos for swagger

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,12 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsString, MinLength } from 'class-validator'
 
 export class RegisterDto {
+  @ApiProperty({
+    example: 'Filipe',
+  })
   @IsString()
   name!: string
 
+  @ApiProperty({
+    example: 'filipe@email.com',
+  })
   @IsEmail()
   email!: string
 
+  @ApiProperty({
+    example: '123456',
+  })
   @IsString()
   @MinLength(6)
   password!: string
--- src/goals/dto/create-goal.dto.ts
+++ src/goals/dto/create-goal.dto.ts
@@ -1,9 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsDateString, IsOptional, IsString } from 'class-validator'
 
 export class CreateGoalDto {
+  @ApiProperty({
+    example: 'ENEM 2027',
+  })
   @IsString()
   title!: string
 
+  @ApiProperty({
+    example: 'Passar no ENEM 2027',
+  })
+  @IsString()
+  description!: string
+
+  @ApiProperty({
+    example: '2027-11-01',
+    required: false,
+  })
   @IsOptional()
   @IsDateString()
   examDate?: string
